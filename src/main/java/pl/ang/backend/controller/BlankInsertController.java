4851287158b16c78959cabd7101c3450b4a9758d
pokorowski <pawelokorowski@gmail.com>
2019-10-28 20:58:54 +0100

    BlankInsert controller endpoints fixes

--- src/main/java/pl/ang/backend/controller/BlankInsertController.java
+++ src/main/java/pl/ang/backend/controller/BlankInsertController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.ang.backend.model.BlankInsertTest;
-import pl.ang.backend.repository.BlankInsertTestRepository;
 import pl.ang.backend.service.BlankInsertTestServiceImpl;
 
 @RestController
@@ -13,17 +12,17 @@ public class BlankInsertController {
     @Autowired
     private BlankInsertTestServiceImpl blankInsertTestService;
 
-    @RequestMapping(value = "/api/saveBlankInsertTest", method = RequestMethod.POST)
+    @RequestMapping(value = "/api/BlankInsertTest", method = RequestMethod.POST)
     public ResponseEntity<?> saveBlankInsertTest(@RequestBody BlankInsertTest blankInsertTest) {
         return ResponseEntity.ok(blankInsertTestService.save(blankInsertTest));
     }
 
-    @RequestMapping(value = "/api/deleteBlankInsertTest/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/api/BlankInsertTest/{id}", method = RequestMethod.DELETE)
     public void deleteBlankInsertTest(@PathVariable long id) {
         blankInsertTestService.delete(id);
     }
 
-    @RequestMapping(value = "/api/editBlankInsertTest/{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/api/BlankInsertTest/{id}", method = RequestMethod.PUT)
     public ResponseEntity<?> editBlankInsertTest(@RequestBody BlankInsertTest blankInsertTest, @PathVariable long id) {
         return ResponseEntity.ok(blankInsertTestService.edit(blankInsertTest, id));
     }

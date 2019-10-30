b0b3cba29960e2adf5e51092b5cf38a0acf90a54
pokorowski <pawelokorowski@gmail.com>
2019-10-30 13:21:23 +0100

    BlankInsertTest repository fixes

--- src/main/java/pl/ang/backend/repository/BlankInsertTestRepository.java
+++ src/main/java/pl/ang/backend/repository/BlankInsertTestRepository.java
@@ -1,7 +1,7 @@
 package pl.ang.backend.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import pl.ang.backend.model.BlankInsertTest
+import pl.ang.backend.model.BlankInsertTest;
 
-public interface BlankInsertTestRepository extends JpaRepository<BlankInsertTest Long> {
+public interface BlankInsertTestRepository extends JpaRepository<BlankInsertTest ,Long> {
 }
+

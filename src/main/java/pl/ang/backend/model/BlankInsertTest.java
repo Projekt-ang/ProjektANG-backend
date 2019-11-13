4db9f17bec15c6897448726dbbf3e7dd95f156fb
pokorowski <pawelokorowski@gmail.com>
2019-11-13 17:30:01 +0100

    Updated Tag and BlankInsertTest models.

--- src/main/java/pl/ang/backend/model/BlankInsertTest.java
+++ src/main/java/pl/ang/backend/model/BlankInsertTest.java
@@ -16,11 +16,20 @@ public class BlankInsertTest {
     @Column(name = "blank_insert_test_id")
     private Long id;
 
-    @Column(name = "text")
+    @Column(name = "text", length = 12000)
     private String text;
+    
+    @Column(name = "name", length = 2047)
+    private String name;
+
+    @Column(name = "author")
+    private String author;
 
     @OneToMany(cascade = {CascadeType.ALL})
     @JoinColumn(name = "blank_insert_test_id")
     private List<BlankSymbol> blankSymbols;
 
+    @ManyToMany(cascade = {CascadeType.ALL})
+    @JoinTable(name = "blank_insert_test_tag", joinColumns = @JoinColumn(name = "blank_insert_test_id"), inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private List<Tag> tags;
 }

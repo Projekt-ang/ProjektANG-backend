fbefc7b5ff3e57d4e73429cf26b3f89c929fbb69
psiewruk <35660839+psiewruk@users.noreply.github.com>
2019-11-17 18:31:32 +0100

    podstawowy mailing, uporzadkowanie application.properties
    uzupelnic haslo do db i maila
    spring.datasource.password=
    spring.mail.password=
    hasla na discordzie

--- src/main/java/pl/ang/backend/controller/TestController.java
+++ src/main/java/pl/ang/backend/controller/TestController.java
@@ -1,11 +1,17 @@
 package pl.ang.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.ang.backend.service.EmailSenderService;
 
 @RestController
 public class TestController {
+
+    @Autowired
+    private EmailSenderService emailSenderService;
 
     @RequestMapping({ "/test" })
     @PreAuthorize("hasRole('USER')")
@@ -17,6 +23,19 @@ public class TestController {
     @PreAuthorize("hasRole('ADMIN')")
     public String adminTest() {
         return "hello world admin";
+    }
+
+    @RequestMapping({ "/mail" })
+    public String emailTest() {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo("kjmasa2@gmail.com");
+        mailMessage.setSubject("TEST MAILA");
+        mailMessage.setFrom("mail.projektang@gmail.com");
+        mailMessage.setText("Testowy mail z kontrolera");
+
+        emailSenderService.sendEmail(mailMessage);
+
+        return "mail";
     }
 
 }
